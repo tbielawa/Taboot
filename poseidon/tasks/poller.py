a373e2492ec70e5bc7e5ac6fd56e97faa8034bfc
Greg Blomquist <gblomqui@redhat.com>
2009-04-23 16:09:59 -0400

    Adding poller task

---
+++ poseidon/tasks/poller.py
@@ -0,0 +1,42 @@
+from poseidon.tasks import command
+import time
+
+class PollTask(BaseTask):
+    """
+    PollTask.  A task that will poll a particular task until the task
+    succeeds or until max_attempts is reached.
+
+    :Parameters:
+        - `task` The task to poll.
+        - `sleep_interval` The number of seconds to wait before trying 
+                           the task again.
+        - `max_attempts` The maximum number of attempts that the task 
+                         should be run.
+        - `fail_task` The task to run when max_attempts has been exhausted.
+                      If no fail_task is provided, then a simple TaskResult
+                      indicating failure is returned.
+    """
+    def __init__(self, task, sleep_interval=5, max_attempts=6, fail_task=None):
+        super(PollTask, self).__init__()
+        self._task = task
+        self._sleep_interval
+        self._max_attempts = max_attempts
+        self._fail_task = fail_task
+
+    def run(self):
+        for x in range(max_attempts): 
+            try:
+                result = self._task.run()
+            except Exception, e:
+                result = TaskResult(self._task, output=repr(e))
+            if result.success:
+                return result
+            time.sleep(sleep_interval)
+
+        # exhausted max_attempts
+        if self._fail_task != None:
+            result = self._fail_task.run()
+        else:
+            # return a "failed" TaskResult, stop executing further tasks
+            return TaskResult(self, success=False, 
+                    output="Max attempts of %s reached running %s" % (max_attempts, repr(self._task)))
