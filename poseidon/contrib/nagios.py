bb29eec751694579f6d2612e2ac277d88fc6328f
John Eckersberg <jeckersb@redhat.com>
2009-05-18 14:22:25 -0400

    pep8 fixes for nagios contrib

--- poseidon/contrib/nagios.py
+++ poseidon/contrib/nagios.py
@@ -1,8 +1,10 @@
 import urllib2
 import urllib
 
-#there's a python egg for this, but it was easier for me to just throw it in the src dir
+#there's a python egg for this, but it was easier for me to just throw
+#it in the src dir
 from urllib2_kerberos import HTTPKerberosAuthHandler
+
 
 class Nagios:
     """
@@ -16,10 +18,13 @@ class Nagios:
     NAGIOS_ADD_COMMENT = '1'
     NAGIOS_DELETE_ALL_COMMENTS = '20'
 
-    def __init__(self, nagios_instance='http://monitor.mgmt.stage.redhat.com/nagios/cgi-bin/cmd.cgi'):
+    def __init__(self, nagios_instance='http://monitor.mgmt.stage.redhat.com' +
+                                       '/nagios/cgi-bin/cmd.cgi'):
         """
-        You need a Kerberos ticket before running this. I'm not going to pass passwords over HTTP.
+        You need a Kerberos ticket before running this. I'm not going
+        to pass passwords over HTTP.
         """
+
         self.nagios_instance = nagios_instance
         opener = urllib2.build_opener(HTTPKerberosAuthHandler())
         urllib2.install_opener(opener)
@@ -38,44 +43,60 @@ class Nagios:
 
     def _call_nagios_alerts(self, host, command):
         """
-        build up the POST call for an enable or disable. Example of what nagios expects to get:
-        cmd_typ=29&cmd_mod=2&host=xmlserver1.app.stage.redhat.com&btnSubmit=Commit
+        build up the POST call for an enable or disable. Example of
+        what nagios expects to get:
+
+        cmd_typ=29&cmd_mod=2&host=xmlserver1.app.stage.redhat.com&
+        btnSubmit=Commit
+
         XXX: need to combine with other call methods
         """
+
         opts = {}
         opts['host'] = host
         opts['cmd_typ'] = command
         opts['cmd_mod'] = '2'
         opts['btnSubmit']='Commit'
         try:
-            response = urllib2.urlopen(self.nagios_instance, urllib.urlencode(opts))
+            response = urllib2.urlopen(self.nagios_instance,
+                                       urllib.urlencode(opts))
         except:
             print "Failed call to Nagios for " + host
             raise
 
     def _call_nagios_comment(self, host, comment, command):
         """
-        build up the POST call for a host comment. Example of what nagios expects to get:
-        cmd_typ=1&cmd_mod=2&host=app02.web.stage.ext.phx2.redhat.com&persistent=on&com_data=test+post&btnSubmit=Commit
+        build up the POST call for a host comment. Example of what
+        nagios expects to get:
+
+        cmd_typ=1&cmd_mod=2&host=app02.web.stage.ext.phx2.redhat.com&
+        persistent=on&com_data=test+post&btnSubmit=Commit
+
         XXX: need to combine with other call methods
         """
+
         opts = {}
-        opts['host'] = host 
+        opts['host'] = host
         opts['cmd_typ'] = command
         opts['cmd_mod'] = '2'
         opts['btnSubmit']='Commit'
         opts['persistent']='on'
         opts['com_data'] = comment
         try:
-            response = urllib2.urlopen(self.nagios_instance, urllib.urlencode(opts))
+            response = urllib2.urlopen(self.nagios_instance,
+                                       urllib.urlencode(opts))
         except:
             print "Failed call to Nagios for " + host
             raise
 
     def _call_nagios_delete_all_comments(self, host, command):
         """
-        build up the POST call for a host comment. Example of what nagios expects to get:
-        cmd_typ=20&cmd_mod=2&host=app02.web.stage.ext.phx2.redhat.com&btnSubmit=Commit
+        build up the POST call for a host comment. Example of what
+        nagios expects to get:
+
+        cmd_typ=20&cmd_mod=2&host=app02.web.stage.ext.phx2.redhat.com&
+        btnSubmit=Commit
+
         XXX: need to combine with other call methods
         """
         opts = {}
@@ -85,9 +106,8 @@ class Nagios:
         opts['btnSubmit']='Commit'
         opts['persistent']='on'
         try:
-            response = urllib2.urlopen(self.nagios_instance, urllib.urlencode(opts))
+            response = urllib2.urlopen(self.nagios_instance,
+                                       urllib.urlencode(opts))
         except:
             print "Failed call to Nagios for " + host
             raise
-
-
