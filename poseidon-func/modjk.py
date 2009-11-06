b695580c8b893dfa0e9644073b18a6255a2852c9
John Eckersberg <jeckersb@redhat.com>
2009-11-06 13:24:54 -0500

    Add list_workers, enable/disable_worker

--- poseidon-func/modjk.py
+++ poseidon-func/modjk.py
@@ -8,16 +8,58 @@ class ModJK(func_module.FuncModule):
     jk = JKManagerBalancerObjectFactory('http://localhost/jkmanage?mime=xml')
 
     def list_balancers(self):
-        return [str(b) for b in self.jk.objects()]
+        return [b.name for b in self.jk.objects()]
+
+    def list_workers(self, balancer):
+        b = jk.get_one(name=balancer)
+        return [(w.name, w.host) for w in b.objects()]
+
+    def enable_worker(self, balancer, worker):
+        w = jk.get_one(name=balancer).get_one(name=worker)
+        w.enable()
+
+    def disable_worker(self, balancer, worker):
+        w = jk.get_one(name=balancer).get_one(name=worker)
+        w.disable()
 
     def register_method_args(self):
         """
         The argument export method
         """
+        balancer = {
+            'type': 'string',
+            'optional': False,
+            'description': 'The name of the desired balancer'
+            }
+        worker = {
+            'type': 'string',
+            'optional': False,
+            'description': 'The name of the desired worker'
+            }
 
         return {
-                'list_balancers':{
-                    'description':'Get list of balancers'
+                'list_balancers': {
+                    'description': 'Get list of balancers'
+                    }
+                'list_workers': {
+                    'args': {
+                        'balancer': balancer
+                        },
+                    'description': 'Get list of workers for a balancer'
+                    }
+                'enable_worker': {
+                    'args': {
+                        'balancer': balancer,
+                        'worker': worker,
+                        },
+                    'description': 'Enable a worker in a balancer'
+                    }
+                'disable_worker': {
+                    'args': {
+                        'balancer': balancer,
+                        'worker': worker,
+                        },
+                    'description': 'Disable a worker in a balancer'
                     }
                 }
 
